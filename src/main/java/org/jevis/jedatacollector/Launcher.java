f5d7cd9ff23b4fe3f3da5b9ef172df3cb1f3d603
broder <broder@broder-730U3E-740U3E>
2015-08-21 17:52:34 +0200

    FEATURE: Driver Reworked

--- src/main/java/org/jevis/jedatacollector/Launcher.java
+++ src/main/java/org/jevis/jedatacollector/Launcher.java
@@ -5,7 +5,6 @@
 package org.jevis.jedatacollector;
 
 import java.util.ArrayList;
-import org.jevis.jedatacollector.service.Request;
 import java.util.List;
 import org.apache.log4j.Appender;
 import org.apache.log4j.FileAppender;
@@ -19,16 +18,15 @@ import org.jevis.api.JEVisException;
 import org.jevis.api.JEVisObject;
 import org.jevis.api.JEVisType;
 import org.jevis.api.sql.JEVisDataSourceSQL;
+import org.jevis.commons.DatabaseHelper;
 import org.jevis.commons.JEVisTypes;
 import org.jevis.commons.cli.JEVisCommandLine;
+import org.jevis.commons.driver.ConverterFactory;
+import org.jevis.commons.driver.DataSourceFactory;
+import org.jevis.commons.driver.ImporterFactory;
+import org.jevis.commons.driver.ParserFactory;
+import org.jevis.commons.driver.datasourcce.DataSource;
 import org.jevis.jedatacollector.CLIProperties.JEVisServerConnectionCLI;
-import org.jevis.jedatacollector.connection.ConnectionFactory;
-import org.jevis.jedatacollector.connection.DataCollectorConnection;
-import org.jevis.jedatacollector.data.DataPoint;
-import org.jevis.commons.parsing.Driver;
-import org.jevis.jedatacollector.service.JevisJobHandler;
-import org.jevis.commons.parsing.LoadingDriver;
-import org.jevis.commons.parsing.ParsingFactory;
 
 /**
  *
@@ -52,81 +50,72 @@ public class Launcher {
         Helper.initializeLogger(JEVisCommandLine.getInstance().getDebugLevel());
 
         JEVisCommandLine cmd = JEVisCommandLine.getInstance();
-        boolean cliJob = cmd.isUsed();
 
         Launcher launcher = new Launcher();
-        List<Request> requestJobs;
 
-        //ToDo starts a new launcher --> with Factory?
-        if (cliJob) {
-            JevisJobHandler handler = new JevisJobHandler();
-            requestJobs = handler.createJobs();
-        } else {
-            launcher.establishConnection();
-            List<Driver> driverList = getDriverList();
-            ConnectionFactory.setDrivers(driverList);
-            ParsingFactory.setDrivers(driverList);
-            JevisJobHandler handler = new JevisJobHandler();
-            requestJobs = handler.createJobs();
-        }
+        launcher.run();
 
-        //hier müssen verschiedene Modi an und abgestellt werden können
-//        boolean cliJob = false;
-        launcher.excecuteRequstsWithThreadsExtend(requestJobs);
-        MDC.put(Launcher.KEY, "-1");
-        Logger.getLogger(Launcher.class.getName()).log(Level.INFO, "########## Finish JEDataCollector #########");
-        System.out.println("Fertig");
     }
 
-    private void excecuteRequstsWithThreadsExtend(List<Request> requestJobs) {
+    private void run() {
+        //connect to the JEVis System
+        establishConnection();
+
+        //load all the possible drivers from the JEVis System. They are specified under 
+        //the JEvis Service Node with the names DataSourceDriver,ParserDriver,ConverterDriver,ImportDriver and the Attributes File, Mainmethod and Objectname.
+        ParserFactory.initializeParser(_client);
+        DataSourceFactory.initializeDataSource(_client);
+        ConverterFactory.initializeConverter(_client);
+        ImporterFactory.initializeImporter(_client);
+
+        //get all data sources from the system, which are enabled
+        List<JEVisObject> dataSources = getEnabledDataSources(_client);
+
+        //execute all data sources
+        excecuteDataSources(dataSources);
+    }
+
+    /**
+     * Run all datasources in Threads. The maximum number of threads is defined
+     * in the JEVis System.
+     *
+     * PROBLEM: with the new structure. Probably not all data sources are able
+     * to handle multiple queries. Any solutions? Maybe without threads?
+     *
+     * @param dataSources
+     */
+    private void excecuteDataSources(List<JEVisObject> dataSources) {
         Logger.getLogger(
-                this.getClass().getName()).log(Level.INFO, "Number of Requests: " + requestJobs.size());
+                this.getClass().getName()).log(Level.INFO, "Number of Requests: " + dataSources.size());
 
         long maxNumberThreads = getNumberOfMaxThreads();
         int threadID = 1;
-        ThreadRequestHandler threadReqHandler = new ThreadRequestHandler(requestJobs);
+        ThreadHandler threadReqHandler = new ThreadHandler(dataSources);
         MDC.remove(Launcher.KEY);
         while (threadReqHandler.hasRequest()) {
-//            int activeCount = Thread.activeCount();
             int activeCount = threadReqHandler.getNumberActiveRequests();
-            if (activeCount < maxNumberThreads && threadReqHandler.hasValidRequest()) {
-                Request currentReq = threadReqHandler.getNextRequest();
-                DataCollectorConnection dataSource = currentReq.getDataSource();
-                initNewAppender("" + threadID, dataSource.getName() + "_ID(" + dataSource.getID() + ").log");
-//                    initNewAppender("" + threadID, "/home/jedc/bin/Thread_" + threadID + ".log");
-//                    Logger.getLogger("Thread_" + currentReq.getDataSource().getID()).log(Level.INFO, "----------------Execute Request-----------------");
+            if (activeCount < maxNumberThreads) {
+                JEVisObject currentDataSourceJevis = threadReqHandler.getNextDataSource();
+                initNewAppender("" + threadID, currentDataSourceJevis.getName() + "_ID(" + currentDataSourceJevis.getID() + ").log");
                 MDC.put(Launcher.KEY, "" + threadID);
-                Logger.getLogger(this.getClass().getName()).log(Level.INFO, "----------------Execute Request-----------------");
-                if (currentReq.getDataSource() != null) {
-//                        Logger.getLogger("Thread_" + currentReq.getDataSource().getID()).log(Level.INFO, "Data Source (ID,Name): (" + currentReq.getDataSource().getID() + "," + currentReq.getDataSource().getName() + ")");
-                    Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Data Source (ID,Name): (" + currentReq.getDataSource().getID() + "," + currentReq.getDataSource().getName() + ")");
-//                Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Datapoints:");
-                    for (DataPoint p : currentReq.getDataPoints()) {
-//                            Logger.getLogger("Thread_" + currentReq.getDataSource().getID()).log(Level.ALL, "Datapoint ID: " + p.getDatapointId());
-                        Logger.getLogger(this.getClass().getName()).log(Level.ALL, "Datapoint ID: " + p.getDatapointId());
-                    }
-                }
-                DataCollector dataCollector = new DataCollector(currentReq);
-                Thread dataCollectionThread = new Thread(dataCollector, currentReq.getDataSource().getName());
-                try {
-                    dataCollectionThread.start();
 
+                Logger.getLogger(this.getClass().getName()).log(Level.INFO, "----------------Execute DataSource-----------------");
+//                DataCollector dataCollector = new DataCollector(currentDataSourceJevis);
+                DataSource dataSource = DataSourceFactory.getDataSource(currentDataSourceJevis);
+                dataSource.initialize(currentDataSourceJevis);
+                Thread dataCollectionThread = new Thread(dataSource, currentDataSourceJevis.getName());
+                try {
+                    //start the data source in a new thread
+                    dataCollectionThread.start();
                 } catch (Exception ex) {
                     Logger.getLogger(Launcher.class.getName()).log(Level.ERROR, ex.getMessage());
                 }
-//                finally {
-
                 threadID++;
                 MDC.remove(Launcher.KEY);
-//                    requestJobs.remove(0);
-//                }
             } else {
                 try {
                     Thread.sleep(10000);
                     System.out.println("thread sleeps");
-                    for (Request req : threadReqHandler.getActiveRequests()) {
-                        System.out.println(req.getDataSource().getName());
-                    }
                 } catch (InterruptedException ie) {
                     System.out.println(ie);
                 }
@@ -170,13 +159,7 @@ public class Launcher {
         return _client;
     }
 
-    public void setJevisClient(JEVisDataSource client) {
-        _client = client;
-    }
-
     private void initNewAppender(String NameForAppender, String Name4LogFile) {
-//        logger = Logger.getLogger(NameForAppender); //NOT DEFAULT BY "logger = Logger.getLogger(TestJob.class);"
-
         FileAppender appender = new FileAppender();
         appender.setLayout(new PatternLayout("[%d{dd MMM yyyy HH:mm:ss}][%c{2}]: %-10m%n"));
         appender.setFile(Name4LogFile);
@@ -189,36 +172,22 @@ public class Launcher {
         logger.addAppender(appender);
     }
 
-    private static List<Driver> getDriverList() {
-        List<Driver> drivers = new ArrayList<Driver>();
+    private List<JEVisObject> getEnabledDataSources(JEVisDataSource client) {
+        List<JEVisObject> enabledDataSources = new ArrayList<JEVisObject>();
         try {
-            JEVisClass collectorClass = getClient().getJEVisClass(JEVisTypes.JEDataCollector.NAME);
-            JEVisClass driverDirClass = getClient().getJEVisClass(JEVisTypes.DriverDirectory.NAME);
-            JEVisClass driverClass = getClient().getJEVisClass(JEVisTypes.Driver.NAME);
-            List<JEVisObject> dataCollector = getClient().getObjects(collectorClass, false);
-            if (dataCollector.size() == 1) {
-                List<JEVisObject> children = dataCollector.get(0).getChildren();
-                JEVisObject driverDirectory = null;
-                for (JEVisObject child : children) {
-                    if (child.getJEVisClass().equals(driverDirClass)) {
-                        driverDirectory = child;
-                        break;
-                    }
-                }
-                if (driverDirectory == null) {
-                    return drivers;
-                }
-                for (JEVisObject child : driverDirectory.getChildren()) {
-                    if (child.getJEVisClass().equals(driverClass)) {
-                        Driver driver = new Driver();
-                        driver.init(child);
-                        drivers.add(driver);
-                    }
+            JEVisClass dataSourceClass = client.getJEVisClass(JEVisTypes.DataServer.NAME);
+            JEVisType enabledType = dataSourceClass.getType(JEVisTypes.DataServer.ENABLE);
+            List<JEVisObject> allDataSources = client.getObjects(dataSourceClass, true);
+            for (JEVisObject dataSource : allDataSources) {
+                Boolean enabled = DatabaseHelper.getObjectAsBoolean(dataSource, enabledType);
+                if (enabled) {
+                    enabledDataSources.add(dataSource);
                 }
             }
         } catch (JEVisException ex) {
             java.util.logging.Logger.getLogger(Launcher.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        return drivers;
+        return enabledDataSources;
     }
+
 }
